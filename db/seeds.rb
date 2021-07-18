1ef92585245e2baf5617aa8ef0f90bfbafe41bec
suey0128 <syui0128@gmail.com>
2021-07-18 16:02:49 -0700

    commit tree def406be79fe1dbf81f3ab9f02cff268ad9721d1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Student.create!(first_name: "John", last_name: "Snow", grade: 3)
+Student.create!(first_name: "Suey", last_name: "Yu", grade: 2)
